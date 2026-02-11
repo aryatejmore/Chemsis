115e77f098b0f3eadbaabbf78dfba37f180187d7
aryatejmore <morearyatej@gmail.com>
2026-02-11 23:23:36 +0530

    Create config.py file with configuration settings for PubChem API and other parameters

---
+++ config.py
@@ -0,0 +1,14 @@
+# Configuration settings for PubChem API
+
+# API Configuration
+PUBCHEM_API_ENDPOINT = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug'
+
+# Data Paths
+DATA_PATH = '/path/to/data'
+
+# Model Parameters
+MODEL_PARAMETERS = {
+    'learning_rate': 0.001,
+    'batch_size': 32,
+    'num_epochs': 100,
+}
